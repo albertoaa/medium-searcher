53083156436a34d1397f0c6a4240e3d83aec9098
Alberto Aragon <alberto@alturasoluciones.com>
2018-07-20 11:48:14 -0500

    Placeholder for components SearchBar and SearchResults

---
+++ components/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+export default class SearchBar extends Component {
+  render() {
+    return(
+      <Text>SearchBar</Text>
+    )
+  }
+}
