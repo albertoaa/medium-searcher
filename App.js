31a04b27a42b63b1c2ddf352eeecbc23422595b9
Alberto Aragon <alberto@alturasoluciones.com>
2018-07-20 17:52:41 -0500

    Adding store

--- App.js
+++ App.js
@@ -4,13 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 import SearchBar from './components/SearchBar/SearchBar';
 import SearchResults from './components/SearchResults/SearchResults';
 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+store.subscribe(() => console.log('store', store.getState()));
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <SearchBar/>
-        <SearchResults/>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <SearchBar/>
+          <SearchResults/>
+        </View>
+      </Provider>
     );
   }
 }
