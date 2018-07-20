fef8fa7eacba18dfec8184dcdfd73dc687a13ab1
Alberto Aragon <alberto@alturasoluciones.com>
2018-07-20 12:56:39 -0500

    Basic layout for SearchBar component

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {TextInput, TouchableOpacity, View} from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+const styles = require('./SearchBarStyles');
 
 export default class SearchBar extends Component {
   render() {
     return(
-      <Text>SearchBar</Text>
+
+      <View style={styles.searchBarContainer}>
+        <TextInput
+          placeholder = 'Enter your search terms'
+          style = {styles.textInputSearch}
+          underlineColorAndroid={'transparent'}
+        />
+        <TouchableOpacity
+          style = {styles.textSearchButton}
+        >
+          <FontAwesome name="search" size={16} color="#000" />
+        </TouchableOpacity>
+      </View>
     )
   }
 }
---
+++ components/SearchBar/SearchBarStyles.js
@@ -0,0 +1,35 @@
+const React = require('react-native');
+const { Dimensions, StyleSheet } = React;
+
+module.exports = StyleSheet.create({
+  searchBarContainer: {
+    width: Dimensions.get('window').width - 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginTop: 40,
+    height: 50,
+    borderBottomWidth: 2,
+    marginVertical: 10,
+    borderColor: 'lightgray',
+    flex: 1
+  },
+  textInputSearch: {
+    flex: 8,
+    borderColor: 'lightgray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 10,
+    height: 40,
+    paddingLeft: 10
+  },
+  textSearchButton: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    height: 40
+  }
+})
